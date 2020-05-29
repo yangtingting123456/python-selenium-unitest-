814bcf98387dad446ab8fed0552d17fb997791a0
yangtingting123456 <3048903923@qq.com>
2020-05-29 11:44:23 +0800

    5555

---
+++ interface_test/interface_test01.py
@@ -0,0 +1,16 @@
+import  requests
+data={
+    "courseType":"3",
+    "courseName":"",
+    "isVip":"false",
+    "isFree":"false",
+    "isPublishTime":"true",
+    "isHeat":"false",
+    "industryId":"-1",
+     "skillId":"-1",
+      "softwareId":"-1",
+       "levelId":"-1",
+      "pageNo":"-1",
+"searchCourseType":"-1"
+
+      }
